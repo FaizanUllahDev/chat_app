b2452f5af0aeb62cb79c85ffb84d9d61a941c173
faizanmehar <42297811+faizanmehar@users.noreply.github.com>
2021-08-31 16:41:13 +0500

    update getAssets

--- pages/getAssets.php
+++ pages/getAssets.php
@@ -17,12 +17,14 @@
                          $rows = mysqli_fetch_assoc($res);
                          
                          echo json_encode($rows);
+                         http_response_code(200);
                         }
                         else
-                        echo 'Error 0';
+                        {echo 'Error 0';
+                           http_response_code(400);}
                      }
                      else
-                     echo 'Error 1';
+                     {echo 'Error 1';http_response_code(400);}
 
       mysqli_close($conn);
   }
