17df396fd564411a4bd951a8700505710591449b
faizanmehar <42297811+faizanmehar@users.noreply.github.com>
2021-09-01 18:13:32 +0500

    Update getAllDoctors.php

--- pages/getAllDoctors.php
+++ pages/getAllDoctors.php
@@ -19,10 +19,12 @@ $q = "SELECT * from doctor INNER JOIN friends ON doctor.number = friends.to_num 
                      
                      
                  }
-                 
+                if(count($data) > 0)
+                 foreach($data as $key => $record )
                  {
-                    $q = "SELECT * from doctor ";
-                    //echo $q;
+                     $n = $record['number'];
+                 
+                    $q = "SELECT * from doctor where number != '$n' ";
                     
                                   if($res = mysqli_query($conn , $q))
                                   {
@@ -38,6 +40,21 @@ $q = "SELECT * from doctor INNER JOIN friends ON doctor.number = friends.to_num 
                                   }
                                 
                  }
+else
+                 $q = "SELECT * from doctor  ";
+                    
+                                  if($res = mysqli_query($conn , $q))
+                                  {
+                                      if(mysqli_num_rows($res) > 0)
+                                      {
+                                        while( $rows = mysqli_fetch_assoc($res))
+                                        array_push($data , $rows);
+                                        
+                                        echo json_encode($data);
+                                        http_response_code(200);
+                                   
+                                      }
+                                  }
               }
               else
               {
