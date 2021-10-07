5b793b125861a309d927b0012e540a56c14276f2
faizanmehar <42297811+faizanmehar@users.noreply.github.com>
2021-10-07 03:40:56 -0700

    update

---
+++ pages/check_header.php
@@ -0,0 +1,38 @@
+<?php
+
+   
+  include 'dbconnection.php' ;
+
+   $token = "";
+   $headers = apache_request_headers();
+   if(isset($headers['Authorization']))
+   {
+        
+        $token =  $headers['Authorization'];
+       
+        $q = "SELECT * FROM token where token = '$token' ";
+        $res = mysqli_query($conn,$q);
+        if(mysqli_num_rows($res) > 0)
+        {
+            $row = mysqli_fetch_assoc($res);
+            $token = $row['token'];
+                // if($token != "")
+                // {
+            echo "Granted !";
+            http_response_code(200);
+                //}
+                // else
+                // {
+                //     echo "Invalid Token !";
+                //     http_response_code(404);
+                // }
+        }
+        else
+        {
+            echo "Invalid Token !";
+            http_response_code(404);
+        }
+   }
+    
+
+?>
--- pages/login.php
+++ pages/login.php
@@ -5,7 +5,7 @@
 //echo $_POST["phone"];
 
 //Generate a random string.
-$token = openssl_random_pseudo_bytes(32);
+$token = openssl_random_pseudo_bytes(256);
 
 //Convert the binary data into hexadecimal representation.
 $token = bin2hex($token);
@@ -69,7 +69,8 @@ $token = bin2hex($token);
         }
       
       }
-    if($count == 0){
+    if($count == 0)
+    {
           $q = "select * from admin where number = '$phone'";
           if ($result=mysqli_query($conn,$q)) 
           if(mysqli_num_rows($result) ==  1 )
@@ -84,29 +85,29 @@ $token = bin2hex($token);
               $count = $count + 1;
           }
          // else $count = $count + 1;
-      }
-  if($count == 0)
-  {
+    }
+        if($count == 0)
+        {
 
-          $q = "select * from pa where number = '$phone'";
-         // echo $q;
-          if ($resultpa=mysqli_query($conn,$q)) 
-          if(mysqli_num_rows($resultpa) ==  1 )
-          {
-             $rowspa = mysqli_fetch_assoc($resultpa) ;     
-            // echo $rowspa;        
-            $data["role"] = "PA";
-            $data["token"] = $token;
-           
-            
-            echo json_encode($data);
-            http_response_code(200);
-              $count = $count + 1;
-          }
-         // else $count = $count + 1;
-  }
+                $q = "select * from pa where number = '$phone'";
+              // echo $q;
+                if ($resultpa=mysqli_query($conn,$q)) 
+                if(mysqli_num_rows($resultpa) ==  1 )
+                {
+                  $rowspa = mysqli_fetch_assoc($resultpa) ;     
+                  // echo $rowspa;        
+                  $data["role"] = "PA";
+                  $data["token"] = $token;
+                
+                  
+                  echo json_encode($data);
+                  http_response_code(200);
+                    $count = $count + 1;
+                }
+              // else $count = $count + 1;
+        }
 
-  if($count ==  0){
+        if($count ==  0){
                echo 'Invalid Phone Number' ;
               http_response_code(401);
               }
@@ -115,7 +116,8 @@ $token = bin2hex($token);
             //   } else {
     //     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     //   }
-      
+      $q = "INSERT INTO token(token,number) VALUES('$token' , '$phone' )";
+      mysqli_query($conn,$q);
       mysqli_close($conn);
   }
   else
